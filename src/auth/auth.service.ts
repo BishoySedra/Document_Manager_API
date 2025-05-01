6ec7f87946196ade9449804577da4803cd61e820
Bishoy Sedra <bishosedra0@gmail.com>
2025-05-01 04:35:24 +0300

    Updated README filegit add .

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -170,7 +170,7 @@ export class AuthService {
 
         const [accessToken, refreshToken] = await Promise.all([
             this.jwtService.signAsync(payload, {
-                secret: this.config.get('JWT_SECRET')
+                secret: this.config.get('JWT_SECRET'),
                 expiresIn: this.config.get('JWT_ACCESS_EXPIRES_IN') || '15m',
             }),
             this.jwtService.signAsync(payload, {
