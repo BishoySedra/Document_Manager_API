590fe7f1941b0e84cd10cdd788d6198757ab954b
Bishoy Sedra <bishosedra0@gmail.com>
2025-05-07 03:27:59 +0300

    Documented Documents Endpoints with Swagger API!

--- src/documents/documents.controller.ts
+++ src/documents/documents.controller.ts
@@ -5,10 +5,13 @@ import * as DocumentsDto from './dto/documents.dto';
 import { JwtGuard } from 'src/auth/guard';
 import { User } from 'src/auth/decorator';
 import { UserPermissions } from 'src/permissions/decorator/user-permissions.decorator';
-import { Permission, Role } from '@prisma/client';
+import { Permission } from '@prisma/client';
 import { DocumentPermissionGuard } from 'src/permissions/guard/document-permission.guard';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
+
 
 @UseGuards(JwtGuard)
+@ApiBearerAuth()
 @Controller('documents')
 export class DocumentsController {
 
@@ -18,6 +21,12 @@ export class DocumentsController {
     // endpoint to upload file
     @Post('upload')
     @UseInterceptors(FileInterceptor('file'))
+    @ApiOperation({ summary: 'Upload a file' })
+    @ApiResponse({ status: HttpStatus.OK, description: 'File uploaded successfully' })
+    @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Invalid or missing file!' })
+    @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: 'Unauthorized' })
+    @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden' })
+    @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, description: 'Internal server error' })
     uploadFile(@UploadedFile() file: Express.Multer.File, @User("id") userId: string) {
         return this.documentService.uploadFile(file, userId);
     }
@@ -26,6 +35,12 @@ export class DocumentsController {
     @Get(':id')
     @UserPermissions(Permission.VIEW)
     @UseGuards(DocumentPermissionGuard)
+    @ApiOperation({ summary: 'Get document by ID' })
+    @ApiResponse({ status: HttpStatus.OK, description: 'Document retrieved successfully' })
+    @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'Document not found' })
+    @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: 'Unauthorized' })
+    @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden' })
+    @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, description: 'Internal server error' })
     getDocumentsById(@Param('id') id: string) {
         return this.documentService.getDocumentsById(id);
     }
@@ -34,6 +49,12 @@ export class DocumentsController {
     @Delete(':id')
     @UserPermissions(Permission.EDIT)
     @UseGuards(DocumentPermissionGuard)
+    @ApiOperation({ summary: 'Delete document by ID' })
+    @ApiResponse({ status: HttpStatus.OK, description: 'Document deleted successfully' })
+    @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'Document not found' })
+    @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: 'Unauthorized' })
+    @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden' })
+    @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, description: 'Internal server error' })
     deleteDocument(@Param('id') id: string) {
         return this.documentService.deleteDocument(id);
     }
@@ -42,6 +63,13 @@ export class DocumentsController {
     @Patch(":id/metadata")
     @UserPermissions(Permission.EDIT)
     @UseGuards(DocumentPermissionGuard)
+    @ApiOperation({ summary: 'Update document metadata' })
+    @ApiResponse({ status: HttpStatus.OK, description: 'Document metadata updated successfully' })
+    @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Invalid metadata' })
+    @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'Document not found' })
+    @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: 'Unauthorized' })
+    @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden' })
+    @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, description: 'Internal server error' })
     addMetaData(@Param('id') id: string, @Body() metaData: DocumentsDto.MetaDataDto, @User('id') createdById: string) {
         return this.documentService.updateMetaData(id, metaData, createdById);
     }
@@ -50,6 +78,12 @@ export class DocumentsController {
     @Get(":id/permissions")
     @UserPermissions(Permission.VIEW)
     @UseGuards(DocumentPermissionGuard)
+    @ApiOperation({ summary: 'Get document permissions' })
+    @ApiResponse({ status: HttpStatus.OK, description: 'Document permissions retrieved successfully' })
+    @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'Document not found' })
+    @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: 'Unauthorized' })
+    @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden' })
+    @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, description: 'Internal server error' })
     getDocumentPermissions(@Param('id') id: string) {
         return this.documentService.getDocumentPermissions(id);
     }
--- src/documents/dto/documents.dto.ts
+++ src/documents/dto/documents.dto.ts
@@ -1,5 +1,7 @@
 import { IsString, IsNotEmpty, IsOptional, IsEnum } from "class-validator";
 import { FileType } from "@prisma/client";
+import { ApiProperty } from "@nestjs/swagger";
+
 
 export enum Tag {
     INVOICE = 'INVOICE',
@@ -14,21 +16,26 @@ export class MetaDataDto {
     @IsString()
     @IsNotEmpty()
     @IsOptional()
+    @ApiProperty({ description: 'Document ID', required: false, example: 'This is a document!' })
     description?: string;
 
     @IsOptional()
     @IsEnum(Tag, { each: true })
+    @ApiProperty({ description: 'Document tags', required: false, enum: Tag, isArray: true })
     tags?: Tag[];
 
     @IsString()
     @IsOptional()
+    @ApiProperty({ description: 'Document title', required: false, example: 'Document Title' })
     title?: string;
 
     @IsString()
     @IsOptional()
+    @ApiProperty({ description: 'Document folder ID', required: false, example: 'Folder ID' })
     folderId?: string;
 
     @IsOptional()
     @IsEnum(FileType)
+    @ApiProperty({ description: 'Document file type', required: false, enum: FileType })
     fileType?: FileType
 }
